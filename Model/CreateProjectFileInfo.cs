1b91b911af288c5f4008814ab4c7ddfd764b0eab
alira13 <30824853+alira13@users.noreply.github.com>
2020-09-21 09:07:55 +0300

    Add initial version of Presenter

---
+++ Model/CreateProjectFileInfo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Text;
+using System.IO;
+
+
+namespace HelloWorld.Model
+{
+    public interface ICreateProjectFileInfo
+    {
+        void GetCreateProjectInfo(string filePath, out FileInfo[] projectFiles, out FileInfo[] templateFiles);
+        bool IsExist(string filePath);
+    }
+
+    public class CreateProjectFileInfo : ICreateProjectFileInfo
+    {
+        public bool IsExist(string filePath)
+        {
+            return File.Exists(filePath);
+        }
+
+        public void GetCreateProjectInfo(string filePath, out FileInfo[] projectFiles, out FileInfo[] templateFiles)
+        {
+            int prjFileNameColumn = 0;
+            int templateFileNameColumn = 1;
+            int worksheetNum = 1;
+            string[,] excelData = ExcelFunc.ReadExcelFile(filePath, worksheetNum);
+
+            projectFiles = new FileInfo[excelData.GetLength(0) - 2];
+            templateFiles = new FileInfo[excelData.GetLength(0) - 2];
+
+            for (int i = 2; i < excelData.GetLength(0); i++)
+            {
+                templateFiles[i-2] = new FileInfo((string)excelData.GetValue(i, templateFileNameColumn));
+                projectFiles[i-2] = new FileInfo((string)excelData.GetValue(i, prjFileNameColumn));
+            }
+        }
+    }
+}
---
+++ Presenter/CreateProjectFilesPresenter.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HelloWorld.View;
+using HelloWorld.Model;
+using System.IO;
+
+namespace HelloWorld.Presenter
+{
+    public class CreateProjectFilesPresenter
+    {
+        private readonly IOpenConfigFileForm _form;
+        private readonly ICreateProjectFileInfo _fileInfo;
+        private readonly IUserMessageFunc _messageFunc;
+
+        public CreateProjectFilesPresenter(IOpenConfigFileForm form, ICreateProjectFileInfo fileInfo, IUserMessageFunc messageFunc)
+        {
+            _form = form;
+            _fileInfo = fileInfo;
+            _messageFunc = messageFunc;
+
+            _form.FileOpenClick += _form_FileOpenClick;
+            _form.SaveClick += _form_SaveClick;
+        }
+
+        private void _form_SaveClick(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void _form_FileOpenClick(object sender, EventArgs e)
+        {
+            try {
+                string filePath = _form.FilePath;
+                bool isExist = _fileInfo.IsExist(filePath);
+                if (!isExist)
+                {
+                    _messageFunc.UserError("Выбранный файл не существует");
+                    return;
+                }
+
+                FileInfo[] projectFiles;
+                FileInfo[] templateFiles;
+
+                _fileInfo.GetCreateProjectInfo(filePath, out projectFiles, out templateFiles);
+            }
+            catch(Exception ex) {
+                _messageFunc.UserError(ex.Message);
+            }
+        }
+    }
+}
